a9a105f7ffc8a76e6c4f658c97bf5a9038ef5713
saulocapistrano <saulocapistrano@gmail.com>
2024-04-22 18:51:24 -0300

    excessão para objeto não encontrado

--- src/main/java/helpdesk/domain/services/TecnicoService.java
+++ src/main/java/helpdesk/domain/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import helpdesk.domain.entities.Tecnico;
 import helpdesk.domain.repositories.TecnicoRepository;
+import helpdesk.domain.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,7 +17,7 @@ public class TecnicoService {
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado id: "+id));
 	}
 	
 }
